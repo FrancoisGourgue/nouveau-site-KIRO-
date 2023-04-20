b8799ca2e3b61bade16a9a8ac57d265ff2860513
root <root@Quasar>
2023-04-20 14:58:11 +0200

    setup rest-api

--- back/api/views.py
+++ back/api/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
 
 # Create your views here.
+
+@api_view(['GET'])
+def getRoutes(request):
+    routes = [
+        {
+            'Endpoint' : '/users/',
+            'method': 'GET',
+            'body': None,
+            'description': ''
+        },
+    ]
+    return Response(routes)
+
+@api_view(['GET'])
+def getUsers(request):
+    members = [
+        [{
+        'name': 'allo'
+        },
+        {
+        'name':'allo'
+        }]
+    ]
+    return Response(members)
+
